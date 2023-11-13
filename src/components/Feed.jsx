7c15ea83840d42876febda7269da650ec2c2635e
Amy Saeed <ammythecreator@gmail.com>
2023-11-13 08:58:38 -0800

    Set up the structure and styling for the video feed part inside of Feed component, and created the video component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+import { Sidebar, Videos } from "./";
 
 const Feed = () => {
   return (
     // this stack got a flexDir for normal devices and md and lower
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       {/* this box, got a height prop, a borderRight and a padding horizontal- two got custom for normal and md screens */}
+      {/* the side bar box */}
       <Box
         sx={{
           height: { sx: "auto", md: "92vh" },
@@ -23,6 +24,20 @@ const Feed = () => {
         >
           Copyright 2023 Miss Screen
         </Typography>
+      </Box>
+
+      {/* the feed box */}
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New <span style={{ color: "#F31503" }}>Videos</span>
+        </Typography>
+        {/* need to fetch the videos */}
+        <Videos videos={[]} />
       </Box>
     </Stack>
   );
