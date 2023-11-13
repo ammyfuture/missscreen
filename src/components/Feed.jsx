1c1035430fc95cffc75947f404d8e28370ab3e0c
Amy Saeed <ammythecreator@gmail.com>
2023-11-13 08:37:23 -0800

    Created a basic structure for the feed and finished  the structure of the side bar being rendered in the feed component. Used material ui for styling and for the icons for both the feed and side bar component, mapped over an array in the constants js file to create the categories in the side bar.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,4 +1,30 @@
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Sidebar from "./Sidebar";
+
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    // this stack got a flexDir for normal devices and md and lower
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      {/* this box, got a height prop, a borderRight and a padding horizontal- two got custom for normal and md screens */}
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+        {/* this txt gets a class name and a variant not sure what that is and extra styling for margin and txt color */}
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2023 Miss Screen
+        </Typography>
+      </Box>
+    </Stack>
+  );
 };
 export default Feed;
