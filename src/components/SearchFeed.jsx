85c99aa8b84eb5daa819da7fe6592bb3e0f90f12
Amy Saeed <ammythecreator@gmail.com>
2023-11-15 02:52:17 -0800

    finished the searchbar functionality

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,4 +1,29 @@
+import { useState, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+import { Videos } from "./";
+import { fetchSearchFromApi } from "../utils/fetchSearchFromApi";
+import { useParams } from "react-router-dom";
+
 const SearchFeed = () => {
-  return <div>SearchFeed</div>;
+  const { searchTerm } = useParams();
+
+  const [videos, setVideos] = useState([]);
+
+  console.log(videos);
+
+  useEffect(() => {
+    fetchSearchFromApi(searchTerm, setVideos);
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        Search Results for:{" "}
+        <span style={{ color: "#F31503" }}>{searchTerm}</span>
+      </Typography>
+      {/* need to fetch the videos */}
+      <Videos videos={videos} />
+    </Box>
+  );
 };
 export default SearchFeed;
